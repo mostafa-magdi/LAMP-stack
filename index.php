4ed7dc1ce44574c3dee445782eb569f447a1b960
mostafa magdi <129240702+mostafa-magdi@users.noreply.github.com>
2023-06-20 21:00:13 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+#<html>
+# <head>
+#   <title>This session about DevOps.</title>
+# </head>
+# <body>
+#         <?php
+#
+#echo "Welcome"
+#
+#?>
+#</body>
+#</html>
+<?php
+$var = 5;
+?>
+<html>
+<head>
+<title>My Page</title>
+</head>
+<body>
+<h2>Your variable was <?php echo $var; ?></h2>
+</body>
+</html>
+
